9a92779e387cc877d2f46553698083503dd54b82
jaypetan <jptann01@gmail.com>
2025-09-12 11:12:30 +0800

    change to tsx

--- app/components/SafeScreen.tsx
+++ app/components/SafeScreen.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-const SafeScreen = ({ children }) => {
+const SafeScreen = ({ children } : { children: React.ReactNode }) => {
     const insets = useSafeAreaInsets();
   return (
     <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom, flex: 1 }}>
